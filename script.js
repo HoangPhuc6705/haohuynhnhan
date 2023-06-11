e6b1985fde6bcdf863909c6b9697899db821a1c2
HoangPhuc6705 <108481791+HoangPhuc6705@users.noreply.github.com>
2023-06-11 21:03:51 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Tắt thông báo
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < 4; i++) {
     document.querySelectorAll('.close')[i].onclick = function() {
         document.querySelectorAll('.notice')[i].style.display = 'none';
     }
@@ -22,3 +22,24 @@ for (let i = 0; i < 3; i++) {
 
 //     lastItem.scrollIntoView({ behavior: "smooth" });
 // }
+
+for (let i = 0; i < 14; i++) {
+    const inner = document.createElement('div');
+    document.querySelectorAll('.NhanHao')[i].appendChild(inner);
+}
+
+// get the elements from the document
+var container = document.querySelector('html');
+var button = document.querySelector('.fullscrhover');
+
+// define a function to toggle fullscreen mode
+function toggleFullscreen() {
+  // check if the document is in fullscreen mode
+  if (document.fullscreenElement) {
+    // exit fullscreen mode
+    document.exitFullscreen();
+  } else {
+    // request fullscreen mode for the container element
+    container.requestFullscreen();
+  }
+}
